2188c9362bb80e41442e592014e166ad22f39b89
Santicode <butzfernando024@gmail.com>
2023-10-22 21:21:02 +0800

    initial output

---
+++ Script/scrollAnimation.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  {
+    threshold: .8, // Trigger the effect when the element is 1.5 times in the viewport while scrolling down
+  }
+);
+
+const hiddenElements = document.querySelectorAll(".hidden");
+[...hiddenElements].forEach((el) => observer.observe(el));
